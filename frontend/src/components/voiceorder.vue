b506e25b92ac3efe64a7304d7f4a2224de86966b
ButterBread12 <skytree9360@gmail.com>
2024-06-19 21:58:03 +0900

    0619

--- frontend/src/components/voiceorder.vue
+++ frontend/src/components/voiceorder.vue
@@ -26,54 +26,77 @@ export default {
   data() {
     return {
       orderList: {},
+      dialog: false,
+      //recognition: null,
     };
   },
   mounted() {
-    EventBus.$on('add-to-cart', this.addToOrder);
-    EventBus.$on('clearAll', this.clearOrderList);
+    //this.startRecognition();
+    EventBus.$on('add-to-cart', this.addToOrder);   // 이벤트 버스로 메뉴의 물품 데이터를 받아옴
+    EventBus.$on('clearAll', this.clearOrderList);  // count 컴포넌트에서 전체삭제 버튼을 누를 경우 이벤트 버스를 받아옴
+    EventBus.$on('itemCount', (item) => {
+      const { name, count } = item;
+      this.increaseCount(name, count)
+    });
   },
   computed: {
     totalPrice() {
-      return Object.values(this.orderList).reduce((total, item) => total + item.price, 0);
+      const total = Object.values(this.orderList).reduce((total, item) => total + item.price, 0);
+      return total
     },
   },
   watch: {
-    totalPrice(newPrice) {
+    totalPrice(newPrice) {  // 합계를 vuex로 전달하는 함수
       this.$store.commit('updateTotalPrice', newPrice);
     },
   },
   methods: {
+    removeItem(name) {
+      this.orderList = this.orderList.filter(item => item.name !== name);
+    },
     clearOrderList() {
-      this.orderList = {};
+      this.orderList = [];
     },
     placeOrder() {
       // 주문 로직 추가
     },
-    addToOrder(item) {
-      if (!this.orderList[item.p_name]) {
+    addToOrder(item) {  // 주문 목록에 물품 데이터를 추가하는 함수
+      if (!this.orderList[item.p_name]) { // orderList에 데이터가 없으면
         this.$set(this.orderList, item.p_name, { name: item.p_name, count: 0, price: 0, unitPrice: item.p_price });
         this.$store.commit('addItem', this.orderList[item.p_name]);
+        console.log("추가된 물품: ", this.orderList[item.p_name])
       }
       this.orderList[item.p_name].count += 1;
       this.orderList[item.p_name].price = this.orderList[item.p_name].unitPrice * this.orderList[item.p_name].count;
     },
-    decreaseCount(itemName) {
+    decreaseCount(itemName) { // 물품 개수를 감소하는 함수
       if (this.orderList[itemName].count > 1) {
         this.orderList[itemName].count -= 1;
         this.orderList[itemName].price = this.orderList[itemName].unitPrice * this.orderList[itemName].count;
+        //this.$store.commit('updateItemPrice', itemName, this.orderList[itemName].price); (필요 없음)
+        console.log("이름: ", itemName, "가격: ", this.orderList[itemName].price)
       } else {
         this.deleteItem(itemName);
       }
     },
-    increaseCount(itemName) {
-      this.orderList[itemName].count += 1;
+
+    increaseCount(itemName, cnt=1) { // 물품 개수를 증가하는 함수
+      this.orderList[itemName].count += cnt;
       this.orderList[itemName].price = this.orderList[itemName].unitPrice * this.orderList[itemName].count;
+      //this.$store.commit('updateItemPrice', {itemName: itemName, price: this.orderList[itemName].price}); (필요 없음)
+      console.log("이름: ", itemName, "가격: ", this.orderList[itemName].price)
     },
-    deleteItem(itemName) {
+
+    deleteItem(itemName) {  // 물품을 삭제하는 함수
       this.$delete(this.orderList, itemName);
       this.$store.commit('delItem', itemName);
       EventBus.$emit('sendItemName', itemName);
     },
+
+    clearOrderList() {  // 주문목록 내의 물품 전체를 삭제하는 함수
+      this.orderList = {};
+    },
+    
   },
 };
 </script>
--- frontend/src/views/AboutView2.vue
+++ frontend/src/views/AboutView2.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
-    <menu-category />
-    <menu-c />
+    <menu-category/>
+    <menu-c/>
     <v-row>
       <v-col>
-        <voiceorder />
+        <voiceorder/>
       </v-col>
       <v-col>
-        <count />
+        <count/>
       </v-col>
     </v-row>
   </div>
 </template>
-
+  
 <script>
 import MenuC from '../components/Menu.vue'
 import voiceorder from '@/components/VoiceOrder.vue'
 import Count from '@/components/Count.vue'
 import menuCategory from '@/components/menuCategory.vue'
-import axios from 'axios'
-import { EventBus } from '../main.js'
+import axios from 'axios';
+import { EventBus } from '../main.js';
 
 export default {
   name: 'App',
@@ -31,132 +31,134 @@ export default {
   },
   data() {
     return {
-      recognition: null
-    }
+      recognition: null,
+    };
+  },
+  created(){
+    //this.speak('주문해주세요');
   },
   mounted() {
-    this.startRecognition()
+    this.startRecognition();
   },
   methods: {
     startRecognition() {
-      this.recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)()
-      this.recognition.lang = 'ko-KR'
-      this.recognition.start()
+      this.recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+      this.recognition.lang = 'ko-KR';
+      this.recognition.start();
 
       this.recognition.onresult = async (event) => {
-        const speechResult = event.results[0][0].transcript.trim()
-        console.log('Result:', speechResult)
-        
-        // 음성 명령 처리
-        this.handleSpeechCommand(speechResult)
-      }
+        const speechResult = event.results[0][0].transcript.trim();
+        console.log('Result:', speechResult);
 
-      this.recognition.onerror = (event) => {
-        console.error('Speech recognition error:', event)
-        alert('Speech recognition error: ' + event.error)  // 사용자에게 오류 메시지를 알림
-      }
+        const response = await fetch('http://localhost:5001/recognize', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ speech: speechResult }),
+        });
 
-      this.recognition.onend = () => {
-        console.log('Speech recognition ended, restarting...')
-        this.recognition.start()
-      }
-    },
-    async speechItem(item, count = 1) {
-      const response = await axios.get(`http://localhost:8000/price/${item}`)
-      const data = { p_name: item, p_price: response.data }
+        //const data = await response.json();
+        //console.log('Order data:', data);
 
-      for (let i = 0; i < count; i++) {
-        EventBus.$emit('add-to-cart', data)
-      }
-    },
-    parseKoreanNumber(text) {
-      const numberMap = {
-        '한': 1,
-        '두': 2,
-        '세': 3,
-        '네': 4,
-        '다섯': 5,
-        '여섯': 6,
-        '일곱': 7,
-        '여덟': 8,
-        '아홉': 9,
-        '열': 10,
-      }
-      return numberMap[text] || 1
-    },
-    handleSpeechCommand(speechResult) {
-      const itemMatch = speechResult.match(/(불고기 버거|더블 불고기 버거|슈슈 버거|더블 빅맥|빅맥 BLT|더블 쿼터파운더 치즈버거|쿼터파운더 치즈버거|맥크리스피 디럭스 버거|맥크리스피 클래식 버거|트리플 치즈 버거|맥스파이시 상하이 버거|1955 버거|맥치킨 모짜렐라 버거|맥치킨|에그 불고기 버거|슈비 버거|베이컨 토마토 디럭스|치즈버거|더블 치즈버거|햄버거)/)
-      const quantityMatch = speechResult.match(/(\d+|한|두|세|네|다섯|여섯|일곱|여덟|아홉|열)개/)
-
-      if (itemMatch && quantityMatch) {
-        const item = itemMatch[0]
-        const quantityText = quantityMatch[1]
-        const quantity = isNaN(quantityText) ? this.parseKoreanNumber(quantityText) : parseInt(quantityText, 10)
-        this.speechItem(item, quantity)
-      } else {
-        // 기존의 카테고리 및 기타 명령어 처리
         if (speechResult.includes('세트') || speechResult.includes('새트')) {
-          EventBus.$emit('sendMenuCategory', 'set')
-        } else if (speechResult.includes('단품') || speechResult.includes('햄버거 단품') || speechResult.includes('단풍')) {
-          EventBus.$emit('sendMenuCategory', 'burger')
-        } else if (speechResult.includes('사이드')) {
-          EventBus.$emit('sendMenuCategory', 'side')
-        } else if (speechResult.includes('음료') || speechResult.includes('음요')) {
-          EventBus.$emit('sendMenuCategory', 'drink')
-        } else if (speechResult.includes('결제')) {
-          EventBus.$emit('openWindow')
-        } else if (speechResult.includes('전체 삭제')) {
-          EventBus.$emit('clearAll')
-        } else if (speechResult.includes('이전')) {
-          EventBus.$emit('prev')
-        } else if (speechResult.includes('다음')) {
-          EventBus.$emit('next')
-        } else {
-          // 단일 항목 처리
-          if (speechResult.includes('더블 불고기 버거') || speechResult.includes('더블불고기버거')) {
+            EventBus.$emit('sendMenuCategory', 'set')
+          } else if (speechResult.includes('단품') || speechResult.includes('햄버거 단품') || speechResult.includes('단풍')) {
+            EventBus.$emit('sendMenuCategory', 'burger')
+          } else if (speechResult.includes('사이드')) {
+            EventBus.$emit('sendMenuCategory', 'side')
+          } else if (speechResult.includes('음료') || speechResult.includes('음요')) {
+            EventBus.$emit('sendMenuCategory', 'drink')
+          } else if (speechResult.includes('결제') || speechResult.includes('골랐어')) {
+            EventBus.$emit('openWindow')
+          } else if (speechResult.includes('전체 삭제')) {
+            EventBus.$emit('deleteOrder')
+          } else if (speechResult.includes('이전')) {
+            EventBus.$emit('prev')
+          } else if (speechResult.includes('다음')) {
+            EventBus.$emit('next')
+          } else if (speechResult.includes('더블 불고기 버거') || speechResult.includes('더블불고기버거')) {
             this.speechItem('더블 불고기 버거')
           } else if (speechResult.includes('슈슈 버거') || speechResult.includes('슈슈버거')) {
             this.speechItem('슈슈 버거')
           } else if (speechResult.includes('더블 빅맥')) {
-            this.speechItem('더블 빅맥')
+            this.speechItem('더블 빅맥');
           } else if (speechResult.includes('빅맥 BLT')) {
-            this.speechItem('빅맥® BLT')
+            this.speechItem('빅맥® BLT');
           } else if (speechResult.includes('더블 쿼터파운더 치즈버거')) {
-            this.speechItem('더블 쿼터파운더® 치즈')
+            this.speechItem('더블 쿼터파운더® 치즈');
           } else if (speechResult.includes('쿼터파운더 치즈버거')) {
-            this.speechItem('쿼터파운더® 치즈 세트버거')
+            this.speechItem('쿼터파운더® 치즈 세트버거');
           } else if (speechResult.includes('맥크리스피 디럭스 버거')) {
-            this.speechItem('맥크리스피™ 디럭스 버거 세트')
+            this.speechItem('맥크리스피™ 디럭스 버거 세트');
           } else if (speechResult.includes('맥크리스피 클래식 버거')) {
-            this.speechItem('맥크리스피™ 클래식 버거')
+            this.speechItem('맥크리스피™ 클래식 버거');
           } else if (speechResult.includes('트리플 치즈 버거')) {
-            this.speechItem('트리플 치즈 버거')
+            this.speechItem('트리플 치즈 버거');
           } else if (speechResult.includes('맥스파이시 상하이 버거')) {
-            this.speechItem('맥스파이시® 상하이 버거')
+            this.speechItem('맥스파이시® 상하이 버거');
           } else if (speechResult.includes('1955 버거')) {
-            this.speechItem('1955® 버거')
+            this.speechItem('1955® 버거');
           } else if (speechResult.includes('맥치킨 모짜렐라 버거')) {
-            this.speechItem('맥치킨® 모짜렐라')
+            this.speechItem('맥치킨® 모짜렐라');
           } else if (speechResult.includes('맥치킨')) {
-            this.speechItem('맥치킨®')
+            this.speechItem('맥치킨®');
           } else if (speechResult.includes('에그 불고기 버거')) {
-            this.speechItem('에그 불고기 버거')
-          } else if (speechResult.includes('불고기 버거')) {
-            this.speechItem('불고기 버거')
+            this.speechItem('에그 불고기 버거');
+          } else if (speechResult.includes('불고기 버거 두 개') || speechResult.includes('불고기버거 두 개')) {
+            this.speechItem('불고기 버거', 2);
+          } else if (speechResult.includes('불고기 버거 세 개') || speechResult.includes('불고기버거 세 개')) {
+            this.speechItem('불고기 버거', 3);
+            this.speak('불고기 버거 세개를 담았습니다.');
+          } else if (speechResult.includes('불고기 버거') || speechResult.includes('불고기버거')) {
+            this.speechItem('불고기 버거');
           } else if (speechResult.includes('슈비 버거')) {
-            this.speechItem('슈비 버거')
+            this.speechItem('슈비 버거');
           } else if (speechResult.includes('베이컨 토마토 디럭스')) {
-            this.speechItem('베이컨 토마토 디럭스')
+            this.speechItem('베이컨 토마토 디럭스');
           } else if (speechResult.includes('치즈버거')) {
-            this.speechItem('치즈버거')
+            this.speechItem('치즈버거');
           } else if (speechResult.includes('더블 치즈버거')) {
-            this.speechItem('더블 치즈버거')
+            this.speechItem('더블 치즈버거');
           } else if (speechResult.includes('햄버거')) {
-            this.speechItem('햄버거')
+            this.speechItem('햄버거');
+          } else if (speechResult.includes('매장')) {
+            EventBus.$emit('select-option', '매장')
+            this.speak('매장을 골랐습니다');
+          } else if (speechResult.includes('포장')) {
+            EventBus.$emit('select-option', '포장')
+          } else if (speechResult.includes('카드')) {
+            EventBus.$emit('select-payment', '카드결제');
+            this.speak('카드로 결제하겠습니다');
+          } else if (speechResult.includes('쿠폰')) {
+            EventBus.$emit('select-payment', '쿠폰결제');
+          } else {
+            this.speak('다시한번 말해주세요');
           }
-        }
-      }
-    }
+      };
+
+      this.recognition.onerror = (event) => {
+        console.error('Speech recognition error:', event);
+      };
+
+      // 음성 인식이 종료될 때 다시 시작
+      this.recognition.onend = () => {
+        this.recognition.start();
+      };
+    },
+    async speechItem(item, cnt=1) {  // 음성인식으로 물품 고를때 이 함수 사용 (코드 수 감소)
+      const response = await axios.get(`http://localhost:8000/price/${item}`);
+      const data = {p_name: item, p_price: response.data};
+      EventBus.$emit('add-to-cart', data);
+      console.log('cnt:', cnt)
+      EventBus.$emit('itemCount', {name: item, count: cnt-1});
+    },
+    speak(message) {
+      const utterance = new SpeechSynthesisUtterance(message);
+      utterance.lang = 'ko-KR';
+      window.speechSynthesis.speak(utterance);
+    },
   }
 }
+  
 </script>
